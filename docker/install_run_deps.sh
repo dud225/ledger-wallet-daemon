af2e606272bbec1c6b8cb39e63cdec8266fd0634
Hervé Werner <herve.werner@ledger.fr>
2021-03-22 12:02:02 +0100

    Install sqlite3 in the Docker image & clean out installed packages

--- docker/install_run_deps.sh
+++ docker/install_run_deps.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-mkdir -p /app/database # for sqlite (can be mount outside the container at runlevel)
+apt-get -q=2 update
 
-# Debug tools untils we have our ledger-stretch-slim image
-apt-get update && apt-get install -yq curl netcat iputils-ping iproute2 lsof procps
-
-# Debug tools untils we have our ledger-stretch-slim image
-apt-get install -yq curl netcat iputils-ping iproute2 lsof procps
+apt-get install -q=2 sqlite3
 
 # Needed when activating PG Support on WD
-apt-get install -yq libpq-dev
+apt-get install -q=2 libpq-dev
 
 # Dependencies of libcore
-apt-get install -yq libxext6 libxrender1 libxtst6
+apt-get install -q=2 libxext6 libxrender1 libxtst6
 
 # Cleanup
 apt-get clean
 rm -rf -- /var/lib/apt/lists/*
+
 exit 0
